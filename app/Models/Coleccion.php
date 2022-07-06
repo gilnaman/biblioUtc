a4261c27682632ebd7bec59edb2a6b704720221a
Danny <74332006+Daniel-Medina@users.noreply.github.com>
2022-07-06 17:20:29 -0500

    Modelo colecciones

---
+++ app/Models/Coleccion.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Coleccion extends Model
+{
+    use HasFactory;
+
+    protected $table = 'colecciones';
+    protected $primarykey = 'id_coleccion';
+
+    public $incrementing = true;
+    public $timestamps = true;
+
+    protected $fillable = [
+        'id_coleccion',
+        'nombre',
+    ];
+
+}
