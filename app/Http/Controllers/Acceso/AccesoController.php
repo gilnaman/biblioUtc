d32131a62a7259437993101082f5ab3cd16d245f
Licsi-can-oxte <licsican@gmail.com>
2022-07-12 12:37:19 -0500

    se agrego el controlador de accesoController y el metodo index

---
+++ app/Http/Controllers/Acceso/AccesoController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Acceso;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Actividad;
+
+class AccesoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $actividades = Actividad::orderBy('actividad', 'asc')->get();
+
+        return view('accesos.vistaAcceso', compact('actividades'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //Respuesta por defecto FALSE
+       
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
