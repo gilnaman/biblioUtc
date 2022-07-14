54ad60e7f628eb0f86b89ba58e51afe59505b418
cruz manuel <77866496+baternail@users.noreply.github.com>
2022-07-14 16:31:35 -0500

    edit.blade.php

--- resources/views/studens/edit.blade.php
+++ resources/views/studens/edit.blade.php
@@ -25,37 +25,22 @@
                             <input type="hidden" name="student_id" value="{{$student->id}}">
                             <div class="row g-3">
                                 <div class="col-3">
-                                    <label for="inputFirstName" class="form-label">First Name<sup><i class="bi bi-asterisk text-primary"></i></sup></label>
+                                    <label for="inputFirstName" class="form-label">nombre<sup><i class="bi bi-asterisk text-primary"></i></sup></label>
                                     <input type="text" class="form-control" id="inputFirstName" name="first_name" placeholder="First Name" required value="{{$student->first_name}}">
                                 </div>
                                 <div class="col-3">
-                                    <label for="inputLastName" class="form-label">Last Name<sup><i class="bi bi-asterisk text-primary"></i></sup></label>
+                                    <label for="inputLastName" class="form-label">apellido<sup><i class="bi bi-asterisk text-primary"></i></sup></label>
                                     <input type="text" class="form-control" id="inputLastName" name="last_name" placeholder="Last Name" required value="{{$student->last_name}}">
                                 </div>
                                 <div class="col-3">
-                                    <label for="inputEmail4" class="form-label">Email<sup><i class="bi bi-asterisk text-primary"></i></sup></label>
+                                    <label for="inputEmail4" class="form-label">matricula<sup><i class="bi bi-asterisk text-primary"></i></sup></label>
                                     <input type="email" class="form-control" id="inputEmail4" name="email" required value="{{$student->email}}">
                                 </div>
                                 <div class="col-3">
-                                    <label for="inputBirthday" class="form-label">Birthday<sup><i class="bi bi-asterisk text-primary"></i></sup></label>
+                                    <label for="inputBirthday" class="form-label">grupo<sup><i class="bi bi-asterisk text-primary"></i></sup></label>
                                     <input type="date" class="form-control" id="inputBirthday" name="birthday" placeholder="Birthday" required value="{{$student->birthday}}">
                                 </div>
-                                <div class="col-3">
-                                    <label for="inputAddress" class="form-label">Address<sup><i class="bi bi-asterisk text-primary"></i></sup></label>
-                                    <input type="text" class="form-control" id="inputAddress" name="address" placeholder="634 Main St" required value="{{$student->address}}">
-                                </div>
-                                <div class="col-3">
-                                    <label for="inputAddress2" class="form-label">Address 2</label>
-                                    <input type="text" class="form-control" id="inputAddress2" name="address2" placeholder="Apartment, studio, or floor" value="{{$student->address2}}">
-                                </div>
-                                <div class="col-2">
-                                    <label for="inputCity" class="form-label">City<sup><i class="bi bi-asterisk text-primary"></i></sup></label>
-                                    <input type="text" class="form-control" id="inputCity" name="city" placeholder="Dhaka..." required value="{{$student->city}}">
-                                </div>
-                                <div class="col-2">
-                                    <label for="inputZip" class="form-label">Zip<sup><i class="bi bi-asterisk text-primary"></i></sup></label>
-                                    <input type="text" class="form-control" id="inputZip" name="zip" required value="{{$student->zip}}">
-                                </div>
+                              
                                 <div class="col-2">
                                     <label for="inputState" class="form-label">Gender<sup><i class="bi bi-asterisk text-primary"></i></sup></label>
                                     <select id="inputState" class="form-select" name="gender" required>
@@ -80,38 +65,7 @@
                                         <option value="AB-" {{($student->blood_type == 'AB-')?'selected':null}}>AB-</option>
                                         <option value="Other" {{($student->blood_type == 'Other')?'selected':null}}>Other</option>
                                     </select>
-                                </div>
-                                <div class="col-3">
-                                    <label for="inputPhone" class="form-label">Phone<sup><i class="bi bi-asterisk text-primary"></i></sup></label>
-                                    <input type="text" class="form-control" id="inputPhone" name="phone" placeholder="+880 01......" required value="{{$student->phone}}">
-                                </div>
-                                <div class="col-3">
-                                    <label for="inputIdCardNumber" class="form-label">Id Card Number<sup><i class="bi bi-asterisk text-primary"></i></sup></label>
-                                    <input type="text" class="form-control" id="inputIdCardNumber" name="id_card_number" placeholder="e.g. 2021-03-01-02-01 (Year Semester Class Section Roll)" required value="{{$promotion_info->id_card_number}}">
-                                </div>
-                            </div>
-                            <div class="row mt-4 g-3">
-                                <h6>Parents' Information</h6>
-                                <div class="col-3">
-                                    <label for="inputFatherName" class="form-label">Father Name<sup><i class="bi bi-asterisk text-primary"></i></sup></label>
-                                    <input type="text" class="form-control" id="inputFatherName" name="father_name" placeholder="Father Name" required value="{{$parent_info->father_name}}">
-                                </div>
-                                <div class="col-3">
-                                    <label for="inputFatherPhone" class="form-label">Father's Phone<sup><i class="bi bi-asterisk text-primary"></i></sup></label>
-                                    <input type="text" class="form-control" id="inputFatherPhone" name="father_phone" placeholder="+880 01......" required value="{{$parent_info->father_phone}}">
-                                </div>
-                                <div class="col-3">
-                                    <label for="inputMotherName" class="form-label">Mother Name<sup><i class="bi bi-asterisk text-primary"></i></sup></label>
-                                    <input type="text" class="form-control" id="inputMotherName" name="mother_name" placeholder="Mother Name" required value="{{$parent_info->mother_name}}">
-                                </div>
-                                <div class="col-3">
-                                    <label for="inputMotherPhone" class="form-label">Mother's Phone<sup><i class="bi bi-asterisk text-primary"></i></sup></label>
-                                    <input type="text" class="form-control" id="inputMotherPhone" name="mother_phone" placeholder="+880 01......" required value="{{$parent_info->mother_phone}}">
-                                </div>
-                                <div class="col-4">
-                                    <label for="inputParentAddress" class="form-label">Address<sup><i class="bi bi-asterisk text-primary"></i></sup></label>
-                                    <input type="text" class="form-control" id="inputParentAddress" name="parent_address" placeholder="634 Main St" required value="{{$parent_info->parent_address}}">
-                                </div>
+                             
                             </div>
                             <div class="row mt-4">
                                 <div class="col-12">
