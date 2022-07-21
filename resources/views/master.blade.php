8ba1da37868d1bbb64acc3937cc37517cfbd5cde
KevinCp7 <106855452+KevinCp7@users.noreply.github.com>
2022-07-21 15:28:15 -0500

    Add files via upload

---
+++ resources/views/master.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Libros</title>
+    <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilosLibros.css">
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/fontawesome.min.css">
+    <script src="{{asset('js/vue.js')}}"></script>
+</head>
+<body>
+    @yield('contenido')
+    @stack('scripts')
+<script type="text/javascript" src="dist/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="dist/js/popper.min.js"></script>
+<script type="text/javascript" src="dist/js/jquery.min.js"></script>
+</body>
+
+</html>
